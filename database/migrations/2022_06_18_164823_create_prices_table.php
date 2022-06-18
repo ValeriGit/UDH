472f81a07649374fe3276972af48ddc857991803
ValeriGit <107764169+ValeriGit@users.noreply.github.com>
2022-06-18 13:35:54 -0500

    first commit

--- database/migrations/2022_06_18_164823_create_prices_table.php
+++ database/migrations/2022_06_18_164823_create_prices_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('prices', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->float('value');
             $table->timestamps();
         });
     }
