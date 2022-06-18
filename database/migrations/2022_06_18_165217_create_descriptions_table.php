e924609f2a8cacafec20e8c422681d71e30d7900
ValeriGit <107764169+ValeriGit@users.noreply.github.com>
2022-06-18 13:42:25 -0500

    first commit

--- database/migrations/2022_06_18_165217_create_descriptions_table.php
+++ database/migrations/2022_06_18_165217_create_descriptions_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('descriptions', function (Blueprint $table) {
             $table->id();
+            $table->text('name');
+            $table->unsignedBigInteger('lesson_id');
+            $table->foreign('lesson_id')->references('id')->on('lessons')->onDelete('cascade');
             $table->timestamps();
         });
     }
