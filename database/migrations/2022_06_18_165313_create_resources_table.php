a859c0a9ab0ff32dd679913ce0b999168b161290
ValeriGit <107764169+ValeriGit@users.noreply.github.com>
2022-06-18 13:43:16 -0500

    first commit

--- database/migrations/2022_06_18_165313_create_resources_table.php
+++ database/migrations/2022_06_18_165313_create_resources_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('resources', function (Blueprint $table) {
             $table->id();
+            $table->string('url');
+            $table->unsignedBigInteger('resourceable_id');
+            $table->string('resourceable_type');
             $table->timestamps();
         });
     }
