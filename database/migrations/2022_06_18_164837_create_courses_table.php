97b27b378b10de250c4c4614f43cc8b8949a7b11
ValeriGit <107764169+ValeriGit@users.noreply.github.com>
2022-06-18 13:36:49 -0500

    first commit

--- database/migrations/2022_06_18_164837_create_courses_table.php
+++ database/migrations/2022_06_18_164837_create_courses_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Course;
 
 return new class extends Migration
 {
@@ -15,6 +16,19 @@ return new class extends Migration
     {
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('subtitle');
+            $table->text('description');
+            $table->enum('status', [Course::ELABORACION, Course::REVISIÓN, Course::PUBLICADO])->default(Course::ELABORACION);
+            $table->string('slug');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('level_id')->nullable();
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->unsignedBigInteger('price_id')->nullable();  
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('level_id')->references('id')->on('levels')->onDelete('set null');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('set null');
+            $table->foreign('price_id')->references('id')->on('prices')->onDelete('set null');
             $table->timestamps();
         });
     }
